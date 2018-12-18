2c46fab9d8a04fe2040b09f1c6470d733db1bac8
TomConn <l.dapo@gmx.net>
2018-12-18 01:57:42 +0100

    Deleted the GAPIRegisterInterface. The reason for this is that the main registry is a standalone and can also be accessed via the EventManager instance.

--- src/main/java/tomconn/growthapi/GrowthAPI.java
+++ src/main/java/tomconn/growthapi/GrowthAPI.java
@@ -1,6 +1,5 @@
 package tomconn.growthapi;
 
-import net.minecraft.block.Block;
 import net.minecraftforge.event.terraingen.SaplingGrowTreeEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,16 +8,12 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import tomconn.growthapi.implementations.EventManager;
 import tomconn.growthapi.implementations.GAPIRegistry;
-import tomconn.growthapi.interfaces.GAPIRegisterInterface;
 import tomconn.growthapi.interfaces.IRegistry;
-
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 @SuppressWarnings("WeakerAccess")
 @Mod(modid = GrowthAPI.modId, name = GrowthAPI.name, version = GrowthAPI.version)
 @Mod.EventBusSubscriber
-public class GrowthAPI implements GAPIRegisterInterface {
+public class GrowthAPI {
 
     public final static String modId = "growthapi";
     public final static String name = "Growth API";
@@ -57,21 +52,5 @@ public class GrowthAPI implements GAPIRegisterInterface {
     @SubscribeEvent
     public static void onSaplingGrowthTreeEvent(SaplingGrowTreeEvent event) {
         eventManager.manage(event);
-    }
-
-
-    @Override
-    public boolean registerCropPre(Class<? extends Block> blockClass, Predicate<BlockEvent.CropGrowEvent.Pre>... requirements) {
-        return registry.registerCropPre(blockClass, requirements);
-    }
-
-    @Override
-    public boolean registerCropPost(Class<? extends Block> blockClass, Consumer<BlockEvent.CropGrowEvent.Post> consumer) {
-        return registry.registerCropPost(blockClass, consumer);
-    }
-
-    @Override
-    public boolean registerSamplingGrowTree(Class<? extends Block> blockClass, Predicate<SaplingGrowTreeEvent>... requirements) {
-        return registry.registerSapling(blockClass, requirements);
     }
 }
