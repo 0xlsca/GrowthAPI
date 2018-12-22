38052eef5c6214b2d8c755ca13730e05ba42698e
TomConn <l.dapo@gmx.net>
2018-12-22 13:00:31 +0100

    IRegistryRetrievalMethods:
    - the Pre and Sapling methods now return lists instead of arrays

--- src/main/java/tomconn/growthapi/interfaces/registry/classbased/IRegistryRetrievalMethods.java
+++ src/main/java/tomconn/growthapi/interfaces/registry/classbased/IRegistryRetrievalMethods.java
@@ -1,9 +1,10 @@
-package tomconn.growthapi.interfaces.registry;
+package tomconn.growthapi.interfaces.registry.classbased;
 
 import net.minecraft.block.Block;
 import net.minecraftforge.event.terraingen.SaplingGrowTreeEvent;
 import net.minecraftforge.event.world.BlockEvent;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -21,7 +22,7 @@ public interface IRegistryRetrievalMethods {
      * @return an array of predicates which are registered with the block class for the
      * {@link net.minecraftforge.event.world.BlockEvent.CropGrowEvent.Pre} event
      */
-    Predicate<BlockEvent.CropGrowEvent.Pre>[] getRequirementsForCropPre(Class<? extends Block> blockClass);
+    List<Predicate<BlockEvent.CropGrowEvent.Pre>> getRequirementsForCropPre(Class<? extends Block> blockClass);
 
     /**
      * Returns the currently registered requirements for the passed class.
@@ -30,7 +31,7 @@ public interface IRegistryRetrievalMethods {
      * @return an array of predicates which are registered with the block class for the
      * {@link SaplingGrowTreeEvent}
      */
-    Predicate<SaplingGrowTreeEvent>[] getRequirementsForSapling(Class<? extends Block> blockClass);
+    List<Predicate<SaplingGrowTreeEvent>> getRequirementsForSapling(Class<? extends Block> blockClass);
 
     /**
      * Returns the currently registered consumer for the passed class.
