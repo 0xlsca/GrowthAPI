ff2c070b4525cfef04a9fd03f92c9cf59cfd4984
TomConn <l.dapo@gmx.net>
2018-12-22 10:34:10 +0100

    EventManager:
    - this class is no longer a singleton

--- src/main/java/tomconn/growthapi/implementations/EventManager.java
+++ src/main/java/tomconn/growthapi/implementations/EventManager.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.eventhandler.Event;
 import tomconn.growthapi.implementations.eventhelpers.CropGrowPostEventHelper;
 import tomconn.growthapi.implementations.eventhelpers.CropGrowPreEventHelper;
 import tomconn.growthapi.implementations.eventhelpers.SaplingGrowTreeEventHelper;
-import tomconn.growthapi.interfaces.registry.IRegistry;
+import tomconn.growthapi.interfaces.registry.classbased.IRegistry;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
@@ -15,22 +15,8 @@ import java.util.function.Predicate;
 /**
  * This class handles {@link net.minecraftforge.event.world.BlockEvent.CropGrowEvent} sub-events and {@link net.minecraftforge.event.terraingen.SaplingGrowTreeEvent}s.
  * <br>
- * This class is a singleton.
  */
 public class EventManager {
-
-    /*
-    Singleton stuff
-     */
-    private static EventManager instance = new EventManager();
-
-    private EventManager() {
-    }
-
-    public static EventManager getInstance() {
-        return instance;
-    }
-
 
     /*
     Attributes go here
